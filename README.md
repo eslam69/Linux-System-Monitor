908d1fc08bb61472a1b3a6c28aa76afef6a62905
mostafa15397 <eslamkhaled64@gmail.com>
2020-06-15 03:17:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Linux-System-Monitor
+
+This is my second project for  udacity c++ nanodegree
+![System Monitor](images/monitor.png)
+
+
+## ncurses
+[ncurses](https://www.gnu.org/software/ncurses/) is a library that facilitates text-based graphical output in the terminal. This project relies on ncurses for display output.
+
+Within the Udacity Workspace, `.student_bashrc` automatically installs ncurses every time you launch the Workspace.
+
+If you are not using the Workspace, install ncurses within your own Linux environment: `sudo apt install libncurses5-dev libncursesw5-dev`
+
+## Make
+This project uses [Make](https://www.gnu.org/software/make/). The Makefile has four targets:
+* `build` compiles the source code and generates an executable
+* `format` applies [ClangFormat](https://clang.llvm.org/docs/ClangFormat.html) to style the source code
+* `debug` compiles the source code and generates an executable, including debugging symbols
+* `clean` deletes the `build/` directory, including all of the build artifacts
+
+## Instructions
+
+1. Clone the project repository: `git clone https://github.com/eslam69/Linux-System-Monitor.git`
+
+2. Build the project: `make build`
+
+3. Run the resulting executable: `./build/monitor`
+![Starting System Monitor](images/starting_monitor.png)
+
+
---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/format.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/linux_parser.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/main.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/ncurses_display.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/process.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/processor.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/src/system.cpp" "/home/eslam/Documents/cpp nd/sysMonitor/Linux System Monitor/build/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
